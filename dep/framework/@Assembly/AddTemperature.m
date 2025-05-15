c37ab8f842e1398ae4046d35169fcff53c04a51b
FE-Berserker <392176462@qq.com>
2025-05-15 12:33:26 +0800

    Update add temperature

--- dep/framework/@Assembly/AddTemperature.m
+++ dep/framework/@Assembly/AddTemperature.m
@@ -1,5 +1,12 @@
-function obj=AddTemperature(obj,partno,T)
+function obj=AddTemperature(obj,partno,T,varargin)
 % Add part Temperature to Assembly
-obj.Temperature=[obj.Temperature;partno,T];
+% Author : Xie Yu
+
+p=inputParser;
+addParameter(p,'type',1);% Type=1 SF Type=2 SFE
+parse(p,varargin{:});
+opt=p.Results;
+
+obj.Temperature=[obj.Temperature;partno,T,opt.type];
 end
 
--- dep/framework/@Assembly/private/Temperatureprint.m
+++ dep/framework/@Assembly/private/Temperatureprint.m
@@ -2,13 +2,20 @@ function Temperatureprint(obj,fid)
 % Print Temperature to ANSYS
 % Author : Xie Yu
 for i=1:size(obj.Temperature,1)
-    Num=obj.Temperature(1,1);
+    Num=obj.Temperature(i,1);
     fprintf(fid, '%s\n','ALLSEL,ALL');
     Sen=strcat('CMSEL,S,Part',num2str(Num));
     fprintf(fid, '%s\n',Sen);
-    fprintf(fid, '%s\n','NSLE,S');
-    Sen=strcat('BF,ALL,TEMP,',num2str(obj.Temperature(1,2)));
-    fprintf(fid, '%s\n',Sen);
+    
+    switch obj.Temperature(i,3)
+        case 1
+            fprintf(fid, '%s\n','NSLE,S');
+            Sen=strcat('BF,ALL,TEMP,',num2str(obj.Temperature(i,2)));
+            fprintf(fid, '%s\n',Sen);
+        case 2
+            Sen=strcat('BFE,ALL,TEMP,',num2str(obj.Temperature(i,2)));
+            fprintf(fid, '%s\n',Sen);
+    end
 end
 fprintf(fid, '%s\n','ALLSEL,ALL');
 end
