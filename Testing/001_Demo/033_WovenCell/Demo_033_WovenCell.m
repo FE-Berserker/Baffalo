8e838482dadabf9d3149a2ae5b4d1d403c435064
FE-Berserker <392176462@qq.com>
2025-06-25 17:26:56 +0800

    Update WovenCell example

--- Testing/001_Demo/033_WovenCell/Demo_033_WovenCell.m
+++ Testing/001_Demo/033_WovenCell/Demo_033_WovenCell.m
@@ -3,8 +3,8 @@ clear
 close all
 % Demo_WovenCell
 % 1. Weave 2D
-
-flag=1;
+% 2. Weave 3D
+flag=2;
 DemoWovenCell(flag);
 
 function DemoWovenCell(flag)
@@ -17,6 +17,19 @@ switch flag
         inputStruct.Fiber=mat{1,1};
         inputStruct.Matrix=mat{2,1};
         paramsStruct.Vf=0.77;
+        W= method.Composite.WovenCell(paramsStruct, inputStruct);
+        W=W.solve();
+        Plot3D(W,'Matrix',0,'SurfaceDistance',1);
+        W=CalProperties(W);
+        disp(W.output.Property)
+    case 2
+        S=RMaterial('Composite');
+        mat=GetMat(S,[32,2]');
+        inputStruct.FileName='Weave2';
+        inputStruct.Dimension=[50,50,50];
+        inputStruct.Fiber=mat{1,1};
+        inputStruct.Matrix=mat{2,1};
+        paramsStruct.Vf=0.75;
         W= method.Composite.WovenCell(paramsStruct, inputStruct);
         W=W.solve();
         Plot3D(W,'Matrix',0,'SurfaceDistance',1);
